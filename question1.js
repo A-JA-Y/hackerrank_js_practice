bd64f5947c08311515a785034b7f61e2bcaf9113
Ajay <mailtoajay7007@gmail.com>
2024-12-07 22:37:54 +0530

    compare the triplets problem

---
+++ question1.js
@@ -0,0 +1,23 @@
+// compare the triplets propblem
+
+let alice = [1, 3, 4];
+let bob = [0, 2, 1];
+
+function compareTriplets(a, b) {
+
+    let aliceScore = 0;
+    let bobScore = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            aliceScore++;
+        } else if (a[i] < b[i]) {
+            bobScore++;
+        }
+    }
+
+    return [aliceScore, bobScore];
+}
+
+console.log(compareTriplets(alice, bob)); 
+
