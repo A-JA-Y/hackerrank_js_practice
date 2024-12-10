ad5d62a800d445c43df17925b496db7402412a01
Ajay <mailtoajay7007@gmail.com>
2024-12-10 21:35:11 +0530

    implementation 1

--- problem4.js
+++ problem4.js
@@ -2,13 +2,18 @@
 
 
 let n = 1012;
-let count = 0;
-let num = n;
-while(num > 0){
-    let digit = num % 10;
-    if(n % digit == 0){
-        count++;
+
+const findDigits = (n) => {
+    let count = 0;
+    let num = n;
+    while(num > 0){
+        let digit = num % 10;
+        if(n % digit == 0){
+            count++;
+        }
+        num = Math.floor(num / 10);
     }
-    num = Math.floor(num / 10);
+    return count;
 }
-console.log(count);
+
+console.log(findDigits(n));
