0ef9eafb4408d4fcc564e0fa715a6e998c8615dc
Ajay <mailtoajay7007@gmail.com>
2024-12-19 22:00:56 +0530

    problem 13: subarray division

---
+++ question13.js
@@ -0,0 +1,17 @@
+// subarray division
+
+const birthday = (s, d, m) => {
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < m; j++) {
+            sum += s[i + j];
+        }
+        if (sum === d) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(birthday([1, 2, 1, 3, 2], 3, 2)); // 2
