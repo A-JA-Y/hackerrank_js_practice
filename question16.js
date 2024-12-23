cc1980917395eae8165b5aa360bc16eeeef78885
Ajay <mailtoajay7007@gmail.com>
2024-12-23 22:03:11 +0530

    problem 16: alternating characters

---
+++ question16.js
@@ -0,0 +1,15 @@
+// alternating characters
+
+
+function alternatingCharacters(s) {
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i + 1]) {
+           
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(alternatingCharacters('Abaabaa')); // 3
